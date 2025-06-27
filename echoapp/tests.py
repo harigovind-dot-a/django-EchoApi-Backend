dcf800cd1fa2f9c856067429d403f75c6817c870
Harigovind Ajith <harigovind@dotproduct.in>
2025-06-27 16:10:47 +0530

    admin and tests
    added the model to the admins panel and wrote a test case to test the api end point

--- echoapp/tests.py
+++ echoapp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Message
+from django.test import Client
+
+class EchoApiTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = '/echo/'
+    
+    def test_post_message(self):
+        data = {'message': 'Testing'}
+        response = self.client.post(self.url, data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('echo', response.json())
+        self.assertEqual(response.json()['echo'], 'Testing')
+
+    def test_get_message(self):
+        Message.objects.create(content='Test message')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('messages', response.json())
+        self.assertEqual(response.json()['messages'][0]['content'], 'Test message')
